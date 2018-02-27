7cb75f1472505eb3d832f629bba61e424aeec8fc
陈志同 <chenzhitong12@hotmail.com>
2018-02-27 13:51:17 +0800

    fixed bug; upload protocal.json

--- LockVerify/Controllers/HomeController.cs
+++ LockVerify/Controllers/HomeController.cs
@@ -14,34 +14,31 @@ namespace LockVerify.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Index(string script)
         {
-            return View();
-        }
-
-        public IActionResult Verify(string script)
-        {
-            if(String.IsNullOrEmpty(script))
-                return View("Index");
+            if (String.IsNullOrEmpty(script))
+                return View();
             if (script.Length != 120)
             {
                 ViewBag.Error = "script lenth error!";
-                return View("Index");
+                return View();
             }
             if (script.Substring(0, 2) != "21" || script.Substring(68, 2) != "04")
             {
                 ViewBag.Error = "script code error!";
-                return View("Index");
+                return View();
             }
             if (script.Substring(0, 2) != "21" || script.Substring(68, 2) != "04")
             {
                 ViewBag.Error = "script code error!";
-                return View("Index");
+                return View();
             }
             if (script.Substring(80, 40) != "a447d01b613011d6ca8cc8092d1700084de8ccd3")
             {
                 ViewBag.Error = "script app call error!";
-                return View("Index");
+                return View();
             }
             var time = script.Substring(70, 8).HexToBytes().ToArray();
 
@@ -50,7 +47,7 @@ namespace LockVerify.Controllers
             ViewBag.Time = ((uint)timestamp).ToDateTime();
             ViewBag.ScriptHash = script.HexToBytes().ToScriptHash();
             ViewBag.Address = Wallet.ToAddress(ViewBag.ScriptHash);
-            return View("Index");
+            return View();
         }
     }
 }
